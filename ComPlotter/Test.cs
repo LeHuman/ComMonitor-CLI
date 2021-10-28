2a9718f0c25e688e509d53ae276ae0120afeb16c
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-27 19:17:15 -0500

    update test

--- ComPlotter/Test.cs
+++ ComPlotter/Test.cs
@@ -3,63 +3,69 @@ using ComPlotter.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Timers;
 
 namespace ComPlotter {
 
     internal class Test {
         private readonly Toaster Toaster;
-        private readonly Timer DataTimer;
         private readonly Random rand = new(0);
         private readonly PlotManager PlotManager;
         private readonly List<PlotGroup> DeletePlotList = new();
         private readonly List<PlotSeries> DeleteSeriesList = new();
-        private readonly Stopwatch Stopwatch = Stopwatch.StartNew();
 
         public Test(PlotManager PlotManager, Toaster Toaster) {
             this.PlotManager = PlotManager;
             this.Toaster = Toaster;
 
-            DataTimer = new();
-            DataTimer.AutoReset = false;
-            DataTimer.Interval = 10;
-            DataTimer.Elapsed += UpdateTestData;
-            DataTimer.Start();
+            _ = Task.Run(UpdateTestData);
         }
 
-        private void UpdateTestData(object sender, EventArgs e) {
-            double s = Math.Sin(Stopwatch.Elapsed.TotalSeconds) + rand.NextDouble() / 2;
-            double r = Math.Round(rand.NextDouble() - .5, 3);
+        private static void NOP(double durationSeconds) {
+            var durationTicks = Math.Round(durationSeconds * Stopwatch.Frequency);
+            var sw = Stopwatch.StartNew();
 
-            foreach (PlotGroup pm in PlotManager.Groups) {
-                foreach (PlotSeries ps in pm.SeriesList) {
-                    if (rand.Next(100) > rand.Next(100)) { ps.Update(Math.Sin(Stopwatch.Elapsed.TotalSeconds)); } else if (rand.Next(100) > rand.Next(100)) { ps.Update(r); } else if (rand.Next(100) > rand.Next(100)) { ps.Update(s + r); } else if (rand.Next(100) > rand.Next(100)) { ps.Update((rand.NextDouble() - 0.5) * rand.NextDouble()); } else if (rand.Next(100) > rand.Next(100)) { ps.Update(s); } else if (rand.Next(100) > 98) { DeleteSeriesList.Add(ps); }
+            while (sw.ElapsedTicks < durationTicks) {
+            }
+        }
+
+        private async Task UpdateTestData() {
+            Stopwatch Stopwatch = Stopwatch.StartNew();
+            while (true) {
+                foreach (PlotGroup pm in PlotManager.Groups) {
+                    foreach (PlotSeries ps in pm.SeriesList) {
+                        if (rand.Next(100000) > 99998) {
+                            DeleteSeriesList.Add(ps);
+                        }
+                    }
+
+                    foreach (PlotSeries ps in DeleteSeriesList) {
+                        pm.RemoveSeries(ps);
+                    }
+
+                    DeleteSeriesList.Clear();
+
+                    if (rand.Next(100) > 85) { pm.Update(rand.Next(5).ToString(), rand.Next(1001) * Math.Sin(Stopwatch.ElapsedMilliseconds)); }
+
+                    if (rand.Next(1000001) == 1000000) {
+                        DeletePlotList.Add(pm);
+                    }
                 }
 
-                if (rand.Next(100) > 85) { pm.CreateSeries(rand.Next(999).ToString()); }
-
-                foreach (PlotSeries ps in DeleteSeriesList) {
-                    pm.RemoveSeries(ps);
+                foreach (PlotGroup pm in DeletePlotList) {
+                    Toaster.WarnToast($"Removed Port : {pm.Name}");
+                    PlotManager.RemoveGroup(pm);
                 }
+                DeletePlotList.Clear();
 
-                DeleteSeriesList.Clear();
-                if (rand.Next(100) >= 99) {
-                    DeletePlotList.Add(pm);
+                if (rand.Next(1000001) >= 999999) {
+                    PlotGroup g = PlotManager.NewGroup(rand.Next(999).ToString());
+                    Toaster.Toast($"New Port : {g.Name}");
                 }
+                if (PlotManager.Groups.Count != 0)
+                    NOP(0.0001);
             }
-
-            foreach (PlotGroup pm in DeletePlotList) {
-                Toaster.WarnToast($"Removed Port : {pm.Name}");
-                PlotManager.RemoveGroup(pm);
-            }
-            DeletePlotList.Clear();
-
-            if (rand.Next(100) > 85) {
-                PlotGroup g = PlotManager.NewGroup(rand.Next(999).ToString());
-                Toaster.Toast($"New Port : {g.Name}");
-            }
-
-            DataTimer.Start();
         }
     }
 }
