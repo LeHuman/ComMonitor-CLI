cef16cb0eb01bc516426aea13111c21394f10feb
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-01-26 21:50:44 -0600

    update libs, copyright, ver

--- Monitor/Properties/AssemblyInfo.cs
+++ Monitor/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LeHuman")]
 [assembly: AssemblyProduct("ComMonitor")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.3.0")]
-[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyVersion("1.3.3.4")]
+[assembly: AssemblyFileVersion("1.3.3.4")]
 [assembly: NeutralResourcesLanguage("English (United States)")]
